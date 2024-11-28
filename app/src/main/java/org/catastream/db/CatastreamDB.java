b2fbab89b514d48bf76e9e766effdddb1243b31a
Deadbeae34149 <deadbear34@gmail.com.com>
2024-11-28 13:11:29 +0700

    update

--- app/src/main/java/org/catastream/db/CatastreamDB.java
+++ app/src/main/java/org/catastream/db/CatastreamDB.java
@@ -1,9 +1,30 @@
 package org.catastream.db;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
 import org.catastream.db.sqlLite.WishList;
 
 @Database(entities = {WishList.class}, version = 1, exportSchema = false)
-public abstract class CatastreamDB {
+public class CatastreamDB extends RoomDatabase {
+    @Override
+    public void clearAllTables() {
+
+    }
+
+    @NonNull
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(@NonNull DatabaseConfiguration databaseConfiguration) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
 }
